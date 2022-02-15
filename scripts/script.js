2a27fe5479de3e201025c85f7bc5cd286c8f88d7
Sophie van der Burg <sophievdburg9@gmail.com>
2022-02-15 15:23:02 +0100

    versie na dag 2

--- scripts/script.js
+++ scripts/script.js
@@ -2,28 +2,49 @@
 const urlAPI = 'https://quote.api.fdnd.nl/v1/quote'
 const quoteLijst = document.querySelector('ul:first-of-type')
 
+const paragraafje = document.querySelector('main > p')
+
+const scrolKnop = document.querySelector('header a')
+const main = document.querySelector('main')
+
 
 // story
 getData()
-
+scrolKnop.addEventListener('click', scrollFunction)
 
 //functions
 function getData(){
-  console.log('GET1')
+  console.log('GET')
   fetch(urlAPI)
     .then(function(response){
       return response.json()
     })
-    .then(function(quotes){
-      console.log("hoi")
-      console.log(quotes.data[0].name)
-      quoteLijst.insertAdjacentHTML('afterbegin', 
-      `<li> 
-        <p> ${quotes.data[0].text}</p>
-        <div>
-            <img src='${quotes.data[0].avatar}' alt='Profielfoto'>
-            <h3>${quotes.data[0].name}</h3>
-        </div
-       </li>`)
-    })
+    .then(quotes => {
+        console.log(quotes.data)
+        paragraafje.innerHTML =  "Here are some quotes to inspire you!"
+        quotes.data.forEach(data => {
+            quoteLijst.insertAdjacentHTML('afterbegin', 
+            `<li> 
+              <p> ${data.text}</p>
+              <div>
+                  <img src='${data.avatar}' alt='Profielfoto'>
+                  <h3>${data.name}</h3>
+              </div
+             </li>`)
+              })
+        })
+    .catch((error) => {
+        console.log("ERROR")
+    });
+}
+
+
+function scrollFunction () {
+    if (document.documentElement.scrollTop < 500) {
+        scrolKnop.scrollIntoView();
+        scrolKnop.classList.add('draai')
+      } else {
+        document.documentElement.scrollTop = 0;
+        scrolKnop.classList.remove('draai')
+      }
 }
