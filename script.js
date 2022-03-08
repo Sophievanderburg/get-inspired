a507b1febd7ded0e77966afb24fc7dc9839deff8
Sophie van der Burg <sophievdburg9@gmail.com>
2022-03-08 14:56:30 +0100

    push

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ import { scrollButton, main, scrollFunction } from './modules/scroll.js'
 import { section, showSectionButton, hideSectionButton, tagsButton, tagsForm, authorButton, authorForm, showOrHideSection, showTagsForm, showAuthorForm } from './modules/sectionInteraction.js';
 
 // Get and render the quotes form the API
-import { urlAPI, quoteList, paragraph, getAndRenderData, renderData } from './modules/getAndRender.js'
+import * as { urlAPI, quoteList, paragraph, getAndRenderData, renderData } from './modules/getAndRender.js'
 
 //forms
 import { checkboxAuthor } from './modules/forms.js'
